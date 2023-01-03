67d710871136793bfad825c1448cff2bd66e0adf
Anastacio Ruiz <anastacior21@gmail.com>
2023-01-02 21:06:59 -0800

    [add] can pull a proffesors rating

--- src/rmp/scrape-rmp.py
+++ src/rmp/scrape-rmp.py
@@ -1,7 +1,8 @@
 import requests
-
-def shovel():
-    pass
+from bs4 import BeautifulSoup
+import json
+import re
+import ast
 
 
 headers = {
@@ -10,18 +11,47 @@ headers = {
                       "Chrome/75.0.3770.142 Safari/537.36"
 }
 
-url = "https://www.ratemyprofessors.com/search/teachers?query=thornton&sid=U2Nob29sLTEwNzQ="
+def format_proff(raw_name):
+    return "%20".join(raw_name)
 
-test = requests.get(url)
+def shovel(raw_proff):
+    fixed_name = raw_proff.split(".")
+    
+    proff_query = format_proff(fixed_name)
+    query_url = f'https://www.ratemyprofessors.com/search.jsp?queryoption=HEADER&queryBy=teacherName&schoolName=University+of+California+Irvine&sid=1074&query={proff_query}'
+    page = requests.get(url=query_url, headers=headers)
+    soup = BeautifulSoup(page.text, "html.parser")
+    scr = str(soup.find_all('script')[10])
+    data = scr[scr.index("window.__RELAY_STORE__ = ") + len("window.__RELAY_STORE__ = "):scr.index("}};") + 3].replace("\\", "")
+    #print(data)
+    json_data = json.loads('{"data":{' + data[data.index("legacyId") - 1:data.index("UC Irvine") + 10] + "}" + "}")
+    #print(json.loads("{data:{" + data[data.index("legacyId") - 1:data.index("UC Irvine") + 10] + "}" + "}"))
+    del json_data['data']['school']#, json_data['U2Nob29sLTEwNzQ=']
+
+    json_data['data']['proff_ID'] = json_data['data']['legacyId']
+
+    del json_data['data']['legacyId']
+    print(json_data)
+
+def gen_link(proff_info):
+    proff_tid = "13200"
+    proff_url = f"https://www.ratemyprofessors.com/professor?tid={proff_tid}"
+
+def get_faculty():
+
+    url = 'https://catalogue.uci.edu/faculty/'
+
+    page = requests.get(url=url, headers=headers)
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    faculty = soup.find("div", {"class" :"clearfix"})
+    alls = faculty.find_all("span", {"class": "name"})
+    for i in alls:
+        print(i.text)
+
+    #print(page.text)
 
 
-#https://www.ratemyprofessors.com/ShowRatings.jsp?tid=2522809
-
-testing = requests.get("https://www.ratemyprofessors.com/campusRatings.jsp?sid=1074")
-#print(requests.get("https://www.ratemyprofessors.com/teachers.jsp?sid=1074").text)
-
-url = "https://www.ratemyprofessors.com/search.jsp/teachers?sid=1074"
-
-
-page = requests.get(url=url, headers=headers)
-print(page.text)
+if __name__ == "__main__":
+    get_faculty()
+    #shovel("A.KRONE MARTINS")
