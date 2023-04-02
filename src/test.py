94c8a44aebb175b17f1c0d49a0b7278de23f5e1c
Ryan <leehahnwoong@gmail.com>
2023-04-02 16:15:03 -0700

    [fix] packaging issues

--- src/test.py
+++ src/test.py
@@ -1,5 +1,5 @@
 from websocAPI import webscrape, utils
 
-r = webscrape.webSocAPI(term=utils.getYear(userYear=2022),dept="I&C SCI", courseNum="33")
+r = webscrape.webSocAPI(term=utils.getYear(userTerm="Spring",userYear=2022),dept="I&C SCI", courseNum="33")
 obj = utils.prettify(r)
 print(obj)
