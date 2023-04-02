7ba91e3fd89e7c0aa9c78b81f65983485e1d377e
Ryan <leehahnwoong@gmail.com>
2023-04-02 13:21:59 -0700

    [fix] minor errors for webscrape

--- src/test.py
+++ src/test.py
@@ -1,7 +1,6 @@
 from websocAPI import webscrape, utils
 import json
 
-
-r = webscrape.webSocAPI(term=utils.getYear(),dept="I&C SCI", courseNum="33")
+r = webscrape.webSocAPI(term=utils.getYear("Spring", 2023),dept="I&C SCI", courseNum="60")
 obj = json.dumps(r, indent=1)
 print(obj)
